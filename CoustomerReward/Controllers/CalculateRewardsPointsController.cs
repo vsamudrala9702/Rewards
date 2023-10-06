0e63d8d446ef81b7475a9349ba3476eb91bed7e2
vinay <vinay.samdurala@gmail.com>
2023-10-06 09:21:33 -0700

    Add project files.

---
+++ CoustomerReward/Controllers/CalculateRewardsPointsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoustomerReward.Models;
+
+namespace CoustomerReward.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculateRewardsPointsController : ControllerBase   {
+
+
+       
+        [HttpPost(Name = "CalculateRewardPoints")]
+        public  int CalculateRewardPoints([FromBody]Transaction transaction)
+        {
+            int rewardPoints = 0;
+
+            if (transaction.Amount > 100)
+            {
+                rewardPoints += (int)((transaction.Amount - 100) * 2);
+                transaction.Amount = 100;
+            }
+
+            if (transaction.Amount > 50)
+            {
+                rewardPoints += (int)(transaction.Amount - 50);
+            }
+
+            return rewardPoints;
+        }
+    }
+}
---
+++ CoustomerReward/Models/Transaction.cs
@@ -0,0 +1,7 @@
+﻿namespace CoustomerReward.Models
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+    }
+}
